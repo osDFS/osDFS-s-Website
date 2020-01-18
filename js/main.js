df029cdacc9eca74424b4eedbe6f2111bbbce92e
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2020-01-18 18:32:24 +0530

    added smooth scroll property

--- js/main.js
+++ js/main.js
@@ -27,3 +27,23 @@ $(function() {
     $('#map').removeClass('scrolloff');
   });
 });
+
+
+//Back to top button
+jQuery(document).ready(function() {
+  var offset = 220;
+  var duration = 500;
+  jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > offset) {
+          jQuery('.myBtn').fadeIn(duration);
+      } else {
+          jQuery('.myBtn').fadeOut(duration);
+      }
+  });
+
+  jQuery('.myBtn').click(function(event) {
+      event.preventDefault();
+      jQuery('html, body').animate({scrollTop: 0}, duration);
+      return false;
+  })
+}); 
